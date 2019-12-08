0b88ffc3077ee0f92c411e5186d1ae7d9f414d52
ywoo21 <ywooo21@gmail.com>
2019-12-08 18:39:18 +0900

    First Commit

--- trip-picker-develop/trip-picker-server/src/main/java/com/picker/trip/model/LoginReq.java
+++
@@ -1,9 +0,0 @@
-package com.picker.trip.model;
-
-import lombok.Data;
-
-@Data
-public class LoginReq {
-    private String email;
-    private String password;
-}
--- trip-picker-develop/trip-picker-server/src/main/java/com/picker/trip/model/enums/PersonalityType.java
+++
@@ -1,5 +0,0 @@
-package com.picker.trip.model.enums;
-
-public enum PersonalityType {
-    NATURE_PERSONAL, EXTREME_PERSONAL, CULTURE_PERSONAL, FAMILY_PERSONAL;
-}
